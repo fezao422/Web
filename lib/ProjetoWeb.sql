ef102e03936108b60cfb0bea98ab606cd3c2f6f1
fezao422 <fezao422@users.noreply.github.com>
2017-12-07 16:44:33 -0200

    sql atualizado

--- lib/ProjetoWeb.sql
+++ lib/ProjetoWeb.sql
@@ -9,7 +9,10 @@ senha varchar(100),
 email varchar(50),
 endereco varchar(20),
 telefone varchar(20),
-foto varchar(100)
+foto varchar(100),
+cep varchar(8),
+cidade varchar(20),
+uf varchar(2)
 );
 
 create table postagem(
